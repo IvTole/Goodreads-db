65db1fa45a6a5d51792c6aaa890ff2b8a9ebaeff
IvTole <toledano.ivan16@gmail.com>
2025-05-12 23:25:25 -0600

    Change schema, ratings table is no longer created and rating variable is now in reviews table. Updated rating range from 0 to 5

--- src/create_db.py
+++ src/create_db.py
@@ -76,22 +76,23 @@ def main():
                                         Location TEXT
                                     ); """
     
-    sql_create_ratings_table = """ CREATE TABLE IF NOT EXISTS Ratings (
-                                        UserID INTEGER,
-                                        BookID INTEGER,
-                                        Rating INTEGER CHECK (Rating >= 1 AND Rating <= 5),
-                                        PRIMARY KEY (UserID, BookID),
-                                        FOREIGN KEY (UserID) REFERENCES Users(UserID),
-                                        FOREIGN KEY (BookID) REFERENCES Books(BookID)
-                                    ); """
+    # sql_create_ratings_table = """ CREATE TABLE IF NOT EXISTS Ratings (
+    #                                     UserID INTEGER,
+    #                                     BookID INTEGER,
+    #                                     Rating INTEGER CHECK (Rating >= 1 AND Rating <= 5),
+    #                                     PRIMARY KEY (UserID, BookID),
+    #                                     FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    #                                     FOREIGN KEY (BookID) REFERENCES Books(BookID)
+    #                                 ); """
     
     sql_create_reviews_table = """ CREATE TABLE IF NOT EXISTS Reviews (
                                         UserID INTEGER,
                                         BookID INTEGER,
-                                        Rating INTEGER CHECK (Rating >= 1 AND Rating <= 5),
-                                        PRIMARY KEY (UserID, BookID),
-                                        FOREIGN KEY (UserID) REFERENCES Users(UserID),
-                                        FOREIGN KEY (BookID) REFERENCES Books(BookID)
+                                        Rating INTEGER CHECK (Rating >= 0 AND Rating <= 5),
+                                        ReviewText TEXT,
+                                        ReviewDate DATETIME,
+                                        Nvotes INTEGER,
+                                        Ncomments INTEGER
                                     ); """
 
     # create a database connection
@@ -118,7 +119,7 @@ def main():
         create_table(conn, sql_create_users_table)
        
         # create ratings table
-        create_table(conn, sql_create_ratings_table)
+        #create_table(conn, sql_create_ratings_table)
         
         # create reviews table
         create_table(conn, sql_create_reviews_table)
