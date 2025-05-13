95aa73510593edaaf30378b7063d1b5c3b7d6dff
IvTole <toledano.ivan16@gmail.com>
2025-05-12 23:27:03 -0600

    Filling new tables Genres and BookAuthors

--- src/db_fill.py
+++ src/db_fill.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-from objects import Author, Book
+from objects import Author, Book, Genre
 import db_functions
-from utils import remove_extra_spaces
+from utils import remove_extra_spaces, list_flatten
 import os
 
 ## Data import
@@ -10,6 +10,7 @@ df = pd.read_csv("../data/Goodreads_books_with_genres.csv")
 df = df.drop(axis=0, index=8180) # parche
 df = df.drop(axis=0, index=11098) # parche
 df['publication_date'] = pd.to_datetime(df['publication_date'], format='%m/%d/%Y')
+df['Author'] = df['Author'].str.replace('"',"'")
 
 # abrir conexion
 db_functions.connect()
@@ -17,7 +18,7 @@ db_functions.connect()
 # Fill authors table
 series_authors = df['Author'] # column of interest
 list_authors = series_authors.str.split(pat="/").values.tolist() # convert to list (separated)
-flat_list_authors = [x2 for x1 in list_authors for x2 in x1] # list flatten
+flat_list_authors = list_flatten(list_authors) # list flatten
 array_authors_unique = pd.Series(flat_list_authors).unique()
 
 for element in array_authors_unique:
@@ -25,6 +26,20 @@ for element in array_authors_unique:
     author = Author(author_name=element)
     db_functions.add_author(author=author)
 print("Authors table filled!")
+
+# Fill genres table
+series_genres = df['genres'].dropna() # column of interest
+list_genres = series_genres.str.split(pat=";").values.tolist() # convert to list (separated)
+flat_list_genres = list_flatten(list_genres) # list flatten
+flat_list_genres_cap = [x.capitalize().strip() for x in flat_list_genres]
+array_genres_unique = pd.Series(flat_list_genres_cap).unique()
+
+for element in array_genres_unique:
+    #element = remove_extra_spaces(element)
+    genre = Genre(genre_name=element)
+    db_functions.add_genre(genre=genre)
+print("Genre table filled!")
+
 
 # Fill books table
 
@@ -43,6 +58,20 @@ for element in book_list:
                 num_pages=element[7])
     db_functions.add_book(book=book)
 print("Book table filled!")
+
+### Fill BookAuthor Table
+
+for isbn in df['isbn']:
+    book_id = db_functions.get_bookid_from_isbn(isbn)
+    
+    for authors_name in df[df['isbn']==isbn]['Author']:
+        for author_name in authors_name.split(sep='/'):
+            author_name = remove_extra_spaces(author_name)
+            author_id = db_functions.get_authorid_from_authorname(author_name)
+            db_functions.add_book_author(book_id=book_id, author_id=author_id)
+
+print("Book Author Table Filled!")
+
 
 ## Close connection to database
 db_functions.close()
