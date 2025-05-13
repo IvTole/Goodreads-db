da21eaae6883ae6fd2b36b462106bb4000bc4dc5
IvTole <toledano.ivan16@gmail.com>
2025-05-12 23:26:15 -0600

    Adding new functions for getting id from books and authors

--- src/db_functions.py
+++ src/db_functions.py
@@ -2,7 +2,7 @@
 
 import sqlite3 as sql
 from contextlib import closing
-from objects import Author, Book
+from objects import Author, Book, Genre
 
 # Variable global que representa el string de conexion
 conn = None
@@ -27,8 +27,15 @@ def add_author(author): # needs an object that represents all of the information
     sql_query = '''INSERT OR IGNORE INTO Authors (AuthorName)
     VALUES (?)'''
     with closing(conn.cursor()) as cursor:
-        cursor.execute(sql_query, (author.author_name,)) # representa al objeto employee
+        cursor.execute(sql_query, (author.author_name,)) # representa al objeto author
         conn.commit()
+
+def get_authorid_from_authorname(author_name): 
+    query = ''' SELECT AuthorID from Authors WHERE AuthorName=?'''
+    with closing(conn.cursor()) as cursor:
+        cursor.execute(query, (author_name,))
+        results = cursor.fetchone()
+    return results['AuthorID']
 
 ### Book functions
 
@@ -38,5 +45,41 @@ def add_book(book): # needs an object that represents all of the information of 
     with closing(conn.cursor()) as cursor:
         cursor.execute(sql_query, (book.bookid, book.title, book.isbn, book.isbn13,
                                    book.language, book.publication_year, book.publisher,
-                                   book.num_pages)) # representa al objeto employee
+                                   book.num_pages)) # representa al objeto book
         conn.commit()
+
+def get_bookid_from_bookname(book_name): 
+    query = ''' SELECT BookID from Books WHERE Title=?'''
+    with closing(conn.cursor()) as cursor:
+        cursor.execute(query, (book_name,))
+        results = cursor.fetchone()
+    return results['BookID']
+
+def get_bookid_from_isbn(isbn): 
+    query = ''' SELECT BookID from Books WHERE ISBN=?'''
+    with closing(conn.cursor()) as cursor:
+        cursor.execute(query, (isbn,))
+        results = cursor.fetchone()
+    return results['BookID']
+
+### Genre functions
+
+def add_genre(genre): # needs an object that represents all of the information of the genre
+    sql_query = '''INSERT OR IGNORE INTO Genres (GenreName)
+    VALUES (?)'''
+    with closing(conn.cursor()) as cursor:
+        cursor.execute(sql_query, (genre.genre_name,)) # representa al objeto genre
+        conn.commit()
+
+## BookAuthor functions
+
+def add_book_author(book_id, author_id): 
+    sql_query = '''INSERT OR IGNORE INTO BookAuthors (BookID, AuthorID)
+    VALUES (?,?)'''
+    with closing(conn.cursor()) as cursor:
+
+        try:
+            cursor.execute(sql_query, (book_id, author_id)) # representa los id's del libro y el autor
+            conn.commit()
+        except:
+            print('Not found')
